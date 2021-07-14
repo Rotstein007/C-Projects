0926c0408768c8b9d84b15ab1965e4b1bb4ee222
Rotstein <74569291+Rotstein007@users.noreply.github.com>
2021-07-14 15:21:21 +0200

    v 1.0

---
+++ heron/cmake-build-debug/CMakeFiles/heron.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heron.dir/main.c.obj"
+  "heron.exe"
+  "heron.exe.manifest"
+  "heron.pdb"
+  "libheron.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/heron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
